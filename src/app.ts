bb3f8207efb6da8900f80ed20bc78cbf78a6a7aa
Shazzadhossensunny <shazzadhossensunny@gmail.com>
2025-06-24 13:12:47 +0600

    readme fiel add

--- src/app.ts
+++ src/app.ts
@@ -9,15 +9,18 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
-// app.use(
-//   cors({
-//     origin: ['http://localhost:5173', 'https://bike-shop-ecru.vercel.app'],
-//     credentials: true,
-//   }),
-// );
+// app.use(cors({ origin: ['http://localhost:5173'], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      'http://localhost:5173',
+      'https://task-management-frontend-kappa-kohl.vercel.app',
+    ],
+    credentials: true,
+  }),
+);
 
 //application routes
 app.use('/api', router);
